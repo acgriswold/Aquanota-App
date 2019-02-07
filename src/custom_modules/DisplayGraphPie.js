26e2e0081f353354b80c9296dece3ea854f04612
Adam Griswold <agriswoldcamp@gmail.com>
2019-02-06 22:52:02 -0500

    updated package.json to include command npm run deploy to deploy to AWS S3

--- src/custom_modules/DisplayGraphPie.js
+++ src/custom_modules/DisplayGraphPie.js
@@ -98,52 +98,10 @@ class DisplayGraphPie extends Component {
                         fill={[
                             {
                                 "match": {
-                                    "id": "ruby"
-                                },
-                                "id": "dots"
-                            },
-                            {
-                                "match": {
-                                    "id": "c"
-                                },
-                                "id": "dots"
-                            },
-                            {
-                                "match": {
-                                    "id": "go"
-                                },
-                                "id": "dots"
-                            },
-                            {
-                                "match": {
-                                    "id": "python"
-                                },
-                                "id": "dots"
-                            },
-                            {
-                                "match": {
-                                    "id": "scala"
+                                    "id": "Chlorate"
                                 },
                                 "id": "lines"
                             },
-                            {
-                                "match": {
-                                    "id": "lisp"
-                                },
-                                "id": "lines"
-                            },
-                            {
-                                "match": {
-                                    "id": "elixir"
-                                },
-                                "id": "lines"
-                            },
-                            {
-                                "match": {
-                                    "id": "javascript"
-                                },
-                                "id": "lines"
-                            }
                         ]}*/
                         legends={[
                             {
