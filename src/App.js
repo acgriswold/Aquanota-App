81ff339dbd9f45655b86939af261093e33fdb9a1
Adam Griswold <agriswoldcamp@gmail.com>
2019-02-12 22:16:28 -0500

    implemented simple side bar navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch, Link} from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Link} from "react-router-dom";
+import SideNav, {MenuIcon} from 'react-simple-sidenav';
 import logo from './img/logo.svg';
 import './css/App.css';
 import Overview from "./custom_pages/Overview";
 import Page404 from "./custom_pages/Page404";
 
 class App extends Component {
+  state = { showNav: false };
+
   render() {
-    return <HashRouter>
+    return <Router>
       <div className = "App">
+        <div className = "header">
+          <MenuIcon onClick={() => this.setState({showNav: true})}/>
+        </div>
+        <SideNav
+          showNav={this.state.showNav}
+          onHideNav={()=>this.setState({showNav:false})}
+          title={<div>Aquanota <img src={logo} width='26' /></div>}
+          titleStyle={{backgroundColor: '#2196F3'}}
+          items={[
+            <Link className="App-link" to="/"> Home </Link>,
+            <Link className="App-link" to="/overview"> Overview </Link>,
+            <Link className="App-link" to="/logout"> Logout </Link>,
+            ]} />
         <Switch>
           <Route exact path="/" component={ Splash } />
           <Route path="/Overview" component={ Overview } />
           <Route component={ Page404 } />
         </Switch>
       </div>
-    </HashRouter>;
+    </Router>;
   }
 }
 
--- src/custom_pages/Overview.js
+++ src/custom_pages/Overview.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import {Link} from "react-router-dom";
 
 import DisplayGraphLine from "../custom_modules/DisplayGraphLine";
 import DisplayGraphPie from "../custom_modules/DisplayGraphPie";
@@ -8,13 +7,8 @@ import DisplayGraphPie from "../custom_modules/DisplayGraphPie";
 class Overview extends Component {
   render() {
     return <div className = "Overview">
-        <code>
-          <Link to="/" className="App-link"> Home </Link>
-        </code>
-        <div>
           <DisplayGraphLine/>
           <DisplayGraphPie/>
-        </div>
       </div>;
   }
 }
